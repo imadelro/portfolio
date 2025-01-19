535eca201a6635692cdcb4687c671ecb6c3804a0
Malena del Rosario <malena.delrosario@student.ateneo.edu>
2025-01-19 13:20:20 +0800

    fix wsgi

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -5,14 +5,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
+"""
 import os
 import sys
-sys.path.append("c:/Users/Malena del Rosario/Documents/Ateneo/Github Portfolio/Django/old vs/portfolio")
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append(r"C:\Users\Malena del Rosario\Documents\Ateneo\Github Portfolio\Django\portfolio")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
