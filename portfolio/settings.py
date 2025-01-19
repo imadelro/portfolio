4e841d97b62f38fe6ebbadb55662d716e858a68f
Malena del Rosario <malena.delrosario@student.ateneo.edu>
2025-01-19 14:17:56 +0800

    pushing to render

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'local','https://portfolio-jqrs.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'local', 'portfolio-jqrs.onrender.com']
 
 
 # Application definition
